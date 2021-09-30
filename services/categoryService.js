65fe5710a389d8e97a8e5eca4eaab730bc3e1fd2
Pavel Mikhaylov <mykhailovpm@gmil.com>
2021-09-30 22:05:19 +0300

    fixed categories need investigation

--- services/categoryService.js
+++ services/categoryService.js
@@ -6,20 +6,29 @@
    */
   actualCategoriesList(activeCat, store) {
     let actualCategoriesList;
+    
     if (activeCat && activeCat.subCategories.length == 0) {
       actualCategoriesList = activeCat.subCategories.map(id =>
-        store.getters.getCategoryById(id)
+        {
+          console.warn('id12321 ', id);
+          return store.getters.getCategoryById(id);
+        }
       );
     } else if (activeCat 
       && activeCat.subCategories 
       && activeCat.subCategories.length > 0) {
-      actualCategoriesList = activeCat.subCategories.map(id =>
-        store.getters.getCategoryById(id)
+      actualCategoriesList = activeCat.subCategories.map(id => {
+
+        console.warn('4444444124 ', id);
+        return store.getters.getCategoryById(id);
+      }
       );
     } else {
       actualCategoriesList = store.state.categories
         .filter(cat => !cat.parentCategory);
     }
-    return actualCategoriesList;
+    // TODO: какая-то ошибка с категориями точно поломанны
+    console.log('actualCategoriesList', actualCategoriesList);
+    return store.state.categories;
   },
 }
